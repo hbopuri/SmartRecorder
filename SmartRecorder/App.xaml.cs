6ef696aac6b9f9c437a012d6cc4d374e2c9ff395
Vaduganathan Pillappan <vaduga@outlook.com>
2020-03-11 12:42:14 +0530

    Added option to pass parameter to differ the quality of Video

--- SmartRecorder/App.xaml.cs
+++ SmartRecorder/App.xaml.cs
@@ -13,5 +13,22 @@ namespace SmartRecorder
     /// </summary>
     public partial class App : Application
     {
+        void App_Startup(object sender, StartupEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            if (e.Args.Count() > 0)
+            {
+                int resolutionId;
+                if (int.TryParse(e.Args[0], out resolutionId))
+                {
+                    mainWindow = new MainWindow(Convert.ToInt32(resolutionId));
+                }
+                else
+                {
+                    ErrorLogger.LogError(null, "Resolution Id for launching SmartRecorder application is not a valied integer!", true, true);
+                }
+            }
+            mainWindow.Show();
+        }
     }
 }
---
+++ SmartRecorder/ErrorLogger.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SmartRecorder
+{
+  public static  class ErrorLogger
+    {
+        public static void LogError(Exception ex, string nonExceptionMessage = null, bool messageRequired = true, bool shutDownRequired = true)
+        {
+            var directoryPath = Path.Combine(Path.GetTempPath(), "Smart Structures", "Smart Recorder");
+            if (!Directory.Exists(directoryPath))
+                Directory.CreateDirectory(directoryPath);
+            var logPath = Path.Combine(directoryPath, "Log.txt");
+            if (!File.Exists(logPath))
+            {
+                File.Create(logPath).Dispose();
+            }
+            using (StreamWriter writer = File.AppendText(logPath))
+            {
+                if (ex != null)
+                {
+                    if (messageRequired)
+                        MessageBox.Show(DateTime.UtcNow + " - " + "exception" + " - " + ex.Message, "Smart Recorder", MessageBoxButton.OK, MessageBoxImage.Error);
+                    writer.WriteLine(DateTime.UtcNow + " - " + "exception" + " - " + ex.Message);
+                }
+                if (!string.IsNullOrEmpty(nonExceptionMessage))
+                {
+                    if (messageRequired)
+                        MessageBox.Show(DateTime.UtcNow + " - " + "non-exception" + " - " + nonExceptionMessage, "Smart Recorder", MessageBoxButton.OK, MessageBoxImage.Error);
+                    writer.WriteLine(DateTime.UtcNow + " - " + "non-exception" + " - " + nonExceptionMessage);
+                }
+            }
+            if (shutDownRequired)
+            {
+                Process.GetCurrentProcess().Kill();
+            }
+        }
+    }
+}
